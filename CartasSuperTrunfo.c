13a8e0f6ad6c2a7c6117a81cc1cb31fa628864ae
brunacomputacoes <brunalvess423@gmail.com>
2025-05-08 00:19:55 +0000

    mudei

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf ("Desafio Super Trunfo!\n");
+    printf ("Primeira Carta\n");
+    char estado;
+    char código;
+    char cidade;
+    int populacao;
+    float área_km_quadrados;
+    float PIB;
+    int pontos_turisticos;
+
+    printf("estado: %s\n", estado);
+    printf("código: %s\n", código);
+    printf("cidade: %s\n", cidade);
+
+
     
     return 0;
 }
